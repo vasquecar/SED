fc6e7c77f11daab7c11d0995827a5ce0cb20ed45
vasquecar <00062619@uca.edu.sv>
2021-11-18 18:28:07 -0600

    avance dos

--- CRUD/app/config/mongodb.config.js
+++ CRUD/app/config/mongodb.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: 'mongodb+srv://c4_vasquez:12345678@cluster0.hib2v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    url: 'mongodb+srv://c4_vasquez:1234567@cluster0.hib2v.mongodb.net/seddb?retryWrites=true&w=majority'
   }
